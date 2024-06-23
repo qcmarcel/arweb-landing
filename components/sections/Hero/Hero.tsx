c959bc920e612a74d427fe8403d5f49d923b0d85
flyyt4 <flayyt33@gmail.com>
2024-06-23 01:17:52 -0300

    Le proporzioni sono state modificate e sono state aggiunte le risposte della sezione Hero.

--- components/sections/Hero/Hero.tsx
+++ components/sections/Hero/Hero.tsx
@@ -1,34 +1,28 @@
+"use client";
 import Image from "next/image";
 import { Button } from "@/components/common/Button";
 import "./Hero.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 function HeroSection() {
   return (
     <section className="Hero">
       <div className="Hero-content">
         <h1>Te ayudamos a crecer y potenciar el éxito de tu negocio</h1>
-        <p className={inter.className}>
+        <p>
           Ofrecemos soluciones completas para llevar tu negocio al siguiente
           nivel
         </p>
-        <div>
+        <div className="Hero-content-buttons">
           <Button buttonType="solid" href="#">
-            <span>Conoce más</span>
+            Conoce más
           </Button>
           <Button buttonType="outline" href="#">
-            <span>Solicita tu demo</span>
+            Solicita tu demo
           </Button>
         </div>
       </div>
       <div className="Hero-image">
         <Image src="/img/HeroMen.png" alt="" width={1792} height={1008} />
-        <div className="Hero-image-triangles">
-          <div id="Hero-image-triangles_1"></div>
-          <div id="Hero-image-triangles_2"></div>
-        </div>
       </div>
     </section>
   );
