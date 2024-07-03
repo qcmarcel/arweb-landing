809fa91c76787dcc8948aba7146b62766a5759d6
David Caycedo <69812733+DcDevRep@users.noreply.github.com>
2024-07-02 23:51:40 -0500

    feat: Section Services finisher

--- components/sections/Services/Services.tsx
+++ components/sections/Services/Services.tsx
@@ -15,7 +15,6 @@ const data = {
 
 function ServicesSection() {
   return (
-    <>
       <section className="Services" data-orden="der-izq">
         <div className="Services-image Services-der">
           <Image
@@ -83,75 +82,6 @@ function ServicesSection() {
           </Card>
         </div>
       </section>
-      <section className="Services" data-orden="izq-der">
-        <div className="Services-image Services-izq">
-          <Image
-            src={data.img.Href}
-            alt={data.img.alt}
-            width={1792}
-            height={1008}
-          />
-        </div>
-        <div className="Services-content Services-der" data-orden="izq-der">
-          <h2>Nuestros servicios</h2>
-          <p className={inter.className}>En que te podemos ayudar</p>
-          <Card cardType="horizontal">
-            <div className="Card-Button">
-              <Button buttonType="icon">
-                <CheckIcon />
-              </Button>
-            </div>
-            <div className="Card-info">
-              <span className={inter.className}>
-                <b>E-commerce</b>
-                <br />
-                Con nuestros servicios podrás contar con una{" "}
-                <b>
-                  tienda en línea
-                  <br /> optimizada
-                </b>{" "}
-                y lista para <b>maximizar tus ventas</b>.
-              </span>
-            </div>
-          </Card>
-          <Card cardType="horizontal">
-            <div className="Card-Button">
-              <Button buttonType="icon">
-                <CheckIcon />
-              </Button>
-            </div>
-            <div className="Card-info">
-              <span className={inter.className}>
-                <b>SEO</b>
-                <br />
-                Nuestro equipo de expertos se asegurará de que tu{" "}
-                <b>e-commerce</b>
-                <br />
-                aparezca en los <b>primeros resultados</b> de los{" "}
-                <b>motores de búsqueda</b>.
-              </span>
-            </div>
-          </Card>
-          <Card cardType="horizontal">
-            <div className="Card-Button">
-              <Button buttonType="icon">
-                <CheckIcon />
-              </Button>
-            </div>
-            <div className="Card-info">
-              <span className={inter.className}>
-                <b>Community Management</b>
-                <br />
-                Nos encargamos de <b>gestionar tus redes sociales,</b> creando
-                una
-                <br />
-                <b>comunidad activa</b> y comprometida con tu marca.
-              </span>
-            </div>
-          </Card>
-        </div>
-      </section>
-    </>
   );
 }
 
